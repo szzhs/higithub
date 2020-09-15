eef432837d4fc935649d8ea53ee4d2b2bd7be1df
szzhs <sz_zhs@126.com>
2020-09-15 17:43:07 +0800

    Add test HelloJava

---
+++ HelloJava.java
@@ -0,0 +1,11 @@
+/* Now I add this file from vim terminal
+* and other 
+*
+*/
+/* test java environment */
+public class HelloJava {
+    public static void main(String[] args) {
+       System.out.println("Hello Java"); 
+    }
+}
+
